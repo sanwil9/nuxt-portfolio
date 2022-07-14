b0997c0f288235ac5a43d338aba2db465328a108
Steven Sanders <sanwil9@gmail.com>
2022-07-14 08:20:44 -0500

    scaffold

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Tutorial/>
+  <main>
+    <AppBar/>
+    <router-view></router-view>
+    <section 
+    class="bg-cover bg-black h-screen"
+    style="background-image: url('https://images.ctfassets.net/wn7ipiv9ue5v/3XvPIRrEsjF3xpeKArg6CN/7b5ed496b02c8f57732dbf625d108a5e/bioshock-tc_keyart_L_1.jpg');">
+    <div 
+    class="h-screen bg-black opacity-25"
+    ></div>
+    </section>
+  </main>
 </template>
 
 <script>
+import AppBarVue from '../components/AppBar.vue'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: [
+    "AppBar"
+  ]
 }
 </script>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [],
+    theme: {
+      textColor: {
+        primary: '#A2DFD4',
+        secondary: '#38FFE7',
+        white: "#FFF",
+        black: "#000",
+      },
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+  };
